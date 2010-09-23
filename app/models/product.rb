61c58ef0bda1721a008ad2b1cf4467f894c79159
OvenAut <oliver@oszko.net>
2010-09-24 00:43:24 +0200

    Task C

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
   #validate :title, :description, :image_url, :presence => true
   validates_presence_of :title ,:description ,:image_url , :message => "can't be blank"
   validates_numericality_of :price, :greater_than_or_equal_to => 0.01, :message => "is not a number!"
   validates_uniqueness_of :title, :message => "must be unique!"  
   validates_format_of :image_url, :with => %r{\.(gif|jpg|png)$}i, :message => "is invalid format"
+  validates_length_of :title, :minimum => 10, :message => "must be more then 10 caracters"
 end
