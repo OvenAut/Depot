f45761f49bbf2194c58c068fd62bc730f2d7ed24
OvenAut <oliver@oszko.net>
2010-09-24 02:32:28 +0200

    Task D

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,10 @@
 class StoreController < ApplicationController
   def index
+    session[:counter] ||= 0
+    session[:counter] += 1 
     @products = Product.all
+    
+      
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,15 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
   default_scope :order => 'title'
+  before_destroy :ensure_not_referenced_by_line_item
+  def ensure_not_referenced_by_line_item
+    if line_items.count.zero?
+      return true
+    else  
+      errors[:base] << "line Item present"
+      return false
+    end
+  end
   #validate :title, :description, :image_url, :presence => true
   validates_presence_of :title ,:description ,:image_url , :message => "can't be blank"
   validates_numericality_of :price, :greater_than_or_equal_to => 0.01, :message => "is not a number!"
